dca93e985ec72e8789a56b714e54a78a9714a268
hariroshan <hariroshanmail@gmail.com>
2023-06-11 16:33:46 +0530

    created new custom element

--- app/app.ts
+++ app/app.ts
@@ -6,10 +6,37 @@ purpose of the file is to pass control to the app’s first module.
 
 import Elm from "./src/Main.elm";
 import { start } from "elm-native-js"
+import { Canvas } from '@nativescript/canvas'
+import { view } from "elm-native-js/src/Native/Constants.bs"
+import { buildHandler, addViewRender } from "elm-native-js/src/Native/Elements.bs"
+import { Screen } from '@nativescript/core/platform'
+
 
 const config = {
   elmModule: Elm,
-  elmModuleName: "Main"
+  elmModuleName: "Main",
+  flags: {width: Screen.mainScreen.widthPixels | 0, height: Screen.mainScreen.heightPixels | 0},
+  customElements: [
+    { tagName: 'canvas'
+    , handler: buildHandler(
+      () => new Canvas(),
+        view,
+        addViewRender
+      )
+    }
+  ],
+  extendCustomElements: (tagName: string, htmlElementClass: any) => {
+    switch(tagName){
+      case 'canvas':
+        return class extends htmlElementClass {
+          getContext() {
+            return this.data.getContext(arguments[0])
+          }
+        }
+      default:
+        return htmlElementClass
+    }
+  }
 }
 
 start(config)
